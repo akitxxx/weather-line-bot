a85412562657574c8705b69967431bb0860c3ad2
aki <>
2019-01-12 19:36:48 +0900

    [up]

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module WeatherLineBot
   class Application < Rails::Application
     config.load_defaults 5.2
 
-    config.time_zone= 'Tokyo'
+    # 設定
+    config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
   end
 end
